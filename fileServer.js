a8cbe85659d5d89ae7695b4415b3db1346e9fb06
Yash Kalia <yashkalia4215@gmail.com>
2024-06-21 22:14:42 +0530

    first commit

---
+++ fileServer.js
@@ -0,0 +1,65 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    // Construct the file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+    // Get the file extension
+    let extname = path.extname(filePath);
+
+    // Set initial content type
+    let contentType = 'text/html';
+
+    // Check the file extension and set the appropriate content type
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    // Read the file
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                // File not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (error, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            } else {
+                // Some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${error.code}`);
+            }
+        } else {
+            // Success
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
